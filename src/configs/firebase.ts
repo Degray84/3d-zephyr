e28a9d9e157ea1df43fd0ed897d0f1c04ee3f1f4
skarmind <katser-s@playrix.com>
2022-08-27 02:21:15 +0300

    feat: add firebase db integration

---
+++ src/configs/firebase.ts
@@ -0,0 +1,8 @@
+export default {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "d-zephyr.firebaseapp.com",
+  projectId: "d-zephyr",
+  storageBucket: "d-zephyr.appspot.com",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
+import firebaseConfig from '@/configs/firebase';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
 
 import App from './App.vue';
 import router from './router';
 
 import './assets/main.css';
+
+const fireapp = initializeApp(firebaseConfig)
+getDatabase(fireapp);
+
+console.log(`Initialize ${!!fireapp ? 'completed' : 'not completed'}`,)
 
 const app = createApp(App);
 
