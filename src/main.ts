4a0976c64b32b24b2bc0b300733211c56249a404
skarmind <katser-s@playrix.com>
2022-08-29 09:56:53 +0300

    fix: test env

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,16 @@ import router from './router';
 
 import './assets/styles/main.scss';
 
+console.log(import.meta.env)
+console.log({
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: "d-zephyr.firebaseapp.com",
+    projectId: "d-zephyr",
+    storageBucket: "d-zephyr.appspot.com",
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+})
+
 const fireapp = initializeApp({
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: "d-zephyr.firebaseapp.com",
