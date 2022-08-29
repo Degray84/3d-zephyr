86640c881b430bf1ef3a1e8a83b9e943eb864834
skarmind <katser-s@playrix.com>
2022-08-29 08:42:06 +0300

    feat: add locales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import firebaseConfig from '@/configs/firebase';
+import i18n from '@/plugins/vue-i18n'
+
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { useAuth } from '@/stores/auth'
 
 
 import App from './App.vue';
@@ -11,7 +15,16 @@ import router from './router';
 import './assets/styles/main.scss';
 
 const fireapp = initializeApp(firebaseConfig)
+
+const auth = getAuth(fireapp);
 getDatabase(fireapp);
+
+onAuthStateChanged(auth, (user) => {
+    const store = useAuth()
+
+    store.fetchUser(user)
+});
+
 
 console.log(`Initialize ${!!fireapp ? 'completed' : 'not completed'}`,)
 
@@ -19,5 +32,7 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(i18n)
+
 
 app.mount('#app');
