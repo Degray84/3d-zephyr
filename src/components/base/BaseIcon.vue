a0fafeaca431e7e0a96abffa155937a2d20cbc28
skarmind <katser-s@playrix.com>
2022-08-29 08:39:10 +0300

    feat: add base icon component

---
+++ src/components/base/BaseIcon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps({
+  small: { type: Boolean, default: false },
+});
+
+const classes = {
+  'base-icon--small': props.small,
+};
+</script>
+
+<template>
+  <span class="base-icon material-icons-outlined" :class="classes"><slot /></span>
+</template>
+
+<style lang="scss" scoped>
+.base-icon {
+  font-size: 24px;
+
+  &--small {
+    font-size: 16px;
+  }
+}
+</style>
