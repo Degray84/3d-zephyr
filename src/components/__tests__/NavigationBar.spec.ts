47d64e305687089cf9bb6c58eb85444f1cc97bd8
skarmind <katser-s@playrix.com>
2022-08-29 09:11:09 +0300

    fix: change import path for navigation

--- src/components/__tests__/NavigationBar.spec.ts
+++ src/components/__tests__/NavigationBar.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 
 import { shallowMount } from '@vue/test-utils'
-import NavigationBar from '../navigation/NavigationBar.vue'
+import NavigationBar from '../common/navigation/TheNavigationBar.vue'
 
 describe('NavigationBar', () => {
   it('NavigationBar renders properly', () => {
