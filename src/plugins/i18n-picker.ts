96c4288490b6453394ab3a0b30f8539c5d238401
skarmind <katser-s@playrix.com>
2022-09-02 23:56:44 +0300

    feat: update plugins structure

---
+++ src/plugins/i18n-picker.ts
@@ -0,0 +1,8 @@
+import { useI18n } from 'vue-i18n';
+
+export default (value: I18nObject) => {
+  const i18n = useI18n();
+  const { locale } = i18n;
+
+  return value[locale.value as keyof I18nObject];
+};
--- src/plugins/vue-i18n.ts
+++ src/plugins/vue-i18n.ts
@@ -1,15 +1,13 @@
-import { createI18n } from 'vue-i18n'
-import ru from '@/locales/ru'
-import en from '@/locales/en'
+import { createI18n } from 'vue-i18n';
+import messages from '@/locales';
 
 const i18n = createI18n({
-    legacy: false,
-    locale: 'ru',
-    fallbackLocale: 'en',
-    messages: {
-        ru,
-        en
-    }
-})
+  legacy: false,
+  locale: 'ru',
+  fallbackLocale: 'en',
+  messages,
+});
 
-export default i18n
+console.log(i18n)
+
+export default i18n;
