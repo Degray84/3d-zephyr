a6933f4b0e294cdafa10f11178667777da8e1821
skarmind <katser-s@playrix.com>
2022-09-02 23:58:27 +0300

    feat: add locales

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,10 +1,11 @@
 export default {
-    to_home: 'Return',
-    home: "Home",
-    about: "About",
-    changeTheme: "Change theme",
-    changeLanguage: "Change language",
-    login: 'Login',
-    logout: 'Logout',
-    hello: 'Hello'
-}
+  to_home: 'Return',
+  home: 'Home',
+  products: 'Catalog',
+  about: 'About',
+  changeTheme: 'Change theme',
+  changeLanguage: 'Change language',
+  login: 'Login',
+  logout: 'Logout',
+  hello: 'Hello',
+};
