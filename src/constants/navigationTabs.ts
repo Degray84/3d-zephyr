2374180db0829187e4e869e6a6a9871ef2ec4069
skarmind <katser-s@playrix.com>
2022-09-11 09:13:50 +0300

    feat: refactor constants

--- src/constants/navigationTabs.ts
+++ src/constants/navigationTabs.ts
@@ -1,24 +1,29 @@
 interface Tab {
-    route: string
-    label: string
-    icon?: string
+  route: string;
+  label: string;
+  icon?: string;
 }
 
 const TABS: Readonly<Tab>[] = [
-    {
-        route: 'home',
-        label: 'home',
-        icon: 'home'
-    },
-    {
-        route: 'products',
-        label: 'products',
-        icon: 'storefront'
-    },
-    {
-        route: 'about',
-        label: 'about',
-        icon: 'person'
-    }
-]
-export default TABS
+  {
+    route: 'home',
+    label: 'home',
+    icon: 'home',
+  },
+  {
+    route: 'products',
+    label: 'products',
+    icon: 'storefront',
+  },
+  {
+    route: 'new_product',
+    label: 'new_product',
+    icon: 'add',
+  },
+  {
+    route: 'about',
+    label: 'about',
+    icon: 'person',
+  },
+];
+export default TABS;
---
+++ src/constants/productTypes.ts
@@ -0,0 +1,11 @@
+export const productTypes = Object.freeze({
+  edgesCutter: {
+    color: 'red',
+  },
+  stampCutter: {
+    color: 'green',
+  },
+  stamp: {
+    color: 'purple',
+  },
+});
